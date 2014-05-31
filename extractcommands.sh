ba464fb8947b81c398098472c01b6016da2f0177
jimhavrilla <semjaavria@gmail.com>
2014-05-31 14:32:32 -0400

    new changes

--- extractcommands.sh
+++ extractcommands.sh
@@ -1,12 +1,8 @@
+#for initial p53 pass:
 (grep ^# ESP6500SI-V2-SSA137.updatedRsIds.chr1.snps_indels.vcf; grep -v ^# -h ESP*.vcf) > foo.vcf; mv foo.vcf ESPALL.vcf
 python extract.py
 #hgTables is pfam from UCSC GenomeBrowser
-cat hgTables | awk '{t=$1;$1=$2;$2=t;t=$2;$2=$3;$3=t;t=$3;$3=$4;$4=t;print;}' > hgTables.bed
-cat hgTables.bed | tr -s " " | tr -s " " "\t" > hg.bed
-cp hg.bed hgTables.bed
-tail -n +2 hgTables.bed > hg.bed
-cp hg.bed hgTables.bed
-sort -k1,1 -k2,2n hgTables.bed > hg.bed
-mv hg.bed hgTables.bed
-bedtools intersect -a hgTables.bed -b gene.bed -sorted > pdomaintp53.bed
-bedtools merge -i <(sort -k1,1 -k2,2n pdomaintp53.bed) -c 4,5 -o distinct | less
+cat hgTables | awk '{t=$1;$1=$2;$2=t;t=$2;$2=$3;$3=t;t=$3;$3=$4;$4=t;print;}' | tr -s " " | tr -s " " "\t" | tail -n +2 | sort -k1,1 -k2,2n > hgTables.bed
+bedtools intersect -a hgTables.bed -b gene.bed -sorted | sort -k1,1 -k2,2n | bedtools merge -i stdin -c 4,5 -o distinct > p53domain.bed
+#for bill's gtf:
+cat Homo_sapiens.chr17.pfam.gtf | awk -F$'\t' '{OFS="\t"} {t=$2;$2=$4;$4=t;t=$3;$3=$5;$5=t;print;}' | awk -F$'\t' '{OFS="\t"} {if (($7 == "-") && ($2 > $3)) {t=$2; $2=$3; $3=t;} print "chr"$0}' | sort -k1,1 -k2,2n | tr -s " " "\t" > chr17.bed
