25b2d2282d5634468a40b83d84164ac4ee11b07d
Ryan Layer <ryan.layer@gmail.com>
2015-10-08 14:47:39 -0600

    check to see if file is there first

--- getfiles.sh
+++ getfiles.sh
@@ -89,19 +89,28 @@ then
 #        -u web_user -pfasta_www pfam27 
 #        < count_human_pfam.sql \
 #            > $DATA/human_pfam.counts
+else
+    echo "FOUND $DATA/human_pfam.counts"
 fi
+
 
 
 # for vcf:
 
 if [ ! -f "$DATA/ExAC.r0.3.sites.vep.vcf.gz" ]
 then
+    echo "CREATING $DATA/ExAC.r0.3.sites.vep.vcf.gz" 
     wget -P $DATA ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3/ExAC.r0.3.sites.vep.vcf.gz
+else
+    echo "FOUND $DATA/ExAC.r0.3.sites.vep.vcf.gz" 
 fi
 
 if [ ! -f "$DATA/VEPEXAC3.vcf.gz" ]
 then
+    echo "CREATING $DATA/VEPEXAC3.vcf.gz"
     perl $SOFTWARE/ensembl-tools-release-81/scripts/variant_effect_predictor/variant_effect_predictor.pl -i $DATA/ExAC.r0.3.sites.vep.vcf.gz --cache --merged --sift b --polyphen b --symbol --numbers --biotype --total_length --allele_number -o $DATA/VEPEXAC3.vcf --vcf --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,ALLELE_NUM --force_overwrite --port 3337 --dir ~/.vep/ --compress "gunzip -c" --offline
+else
+    echo "FOUND $DATA/VEPEXAC3.vcf.gz" 
 fi
 
 # for exons: 
@@ -114,6 +123,7 @@ fi
 
 if [ ! -f "$DATA/exons.bed" ]
 then
+    echo "CREATING $DATA/exons.bed"
     zcat < $DATA/Homo_sapiens.GRCh37.75.gtf.gz \
         | grep protein_coding$'\t'exon  \
         | perl -pe 's/protein_coding\texon\t//g' \
@@ -121,10 +131,13 @@ then
         | perl gtf2bed.pl \
         | sort -k10,10 \
         > $DATA/exons.bed
+else
+    echo "FOUND $DATA/exons.bed"
 fi
 
 if [ ! -f "$DATA/utrs.bed" ]
 then
+    echo "CREATING $DATA/utrs.bed"
     zcat < $DATA/Homo_sapiens.GRCh37.75.gtf.gz \
         | grep protein_coding$'\t'UTR \
         | perl -pe 's/protein_coding\tUTR\t//g' \
@@ -132,25 +145,93 @@ then
         | perl gtf2bed.pl \
         | sort -k10,10 \
         > $DATA/utrs.bed
+else
+    echo "FOUND $DATA/utrs.bed"
 fi
 
-python filterutrs.py $DATA/exons.bed $DATA/utrs.bed > $DATA/GRCh37.bed
+if [ ! -f "$DATA/exons_sans_utrs.bed" ]
+then
+    echo "CREATING $DATA/exons_sans_utrs.bed" 
+    python filterutrs.py $DATA/exons.bed $DATA/utrs.bed > $DATA/exons_sans_utrs.bed
+
+    echo "VALIDATING $DATA/exons_sans_utrs.bed"
+
+    A=`cat $DATA/exons_sans_utrs.bed \
+        | grep -v "^#" \
+        | grep transcript_id \
+        | cut -f 7 \
+        | cut -d ";" -f2 \
+        | uniq \
+        | sort -u \
+        | wc -l`
+
+    B=`cat $DATA/exons.bed \
+        | grep -v "^#" \
+        | grep transcript_id \
+        | cut -f 7 \
+        | cut -d ";" -f2 \
+        | uniq \
+        | sort -u \
+        | wc -l`
+
+    if [ "$A" -ne "$B" ]
+    then
+        echo "Mismatch between transcripts in $DATA/exons.bed ($A) and $DATA/exons_sans_utrs.bed ($B)"
+        exit
+    else
+        echo "Transcripts match between in $DATA/exons.bed ($A) and $DATA/exons_sans_utrs.bed ($B)"
+    fi
+else
+    echo "FOUND $DATA/exons_sans_utrs.bed" 
+fi
 
 # for correct transcripts:
 
-wget -P $DATA http://apprisws.bioinfo.cnio.es/pub/current_release/ensembl_datafiles/species/homo_sapiens/GRCh37/appris_data.principal.txt
+if [ ! -f "$DATA/appris_data.principal.txt" ]
+then
+    echo "CREATING $DATA/appris_data.principal.txt"
+
+    wget -P $DATA http://apprisws.bioinfo.cnio.es/pub/current_release/ensembl_datafiles/species/homo_sapiens/GRCh37/appris_data.principal.txt
+
+else
+    echo "FOUND $DATA/appris_data.principal.txt"
+fi
 
 # for vcf coverage:
 
-for chrom in {1..22} X Y ; do wget -P $DATA ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3/coverage/Panel.chr$chrom.coverage.txt.gz; done
-gzcat $DATA/Panel.chr*.coverage.txt.gz | awk '/^#/ {sub(/#.*/,"");getline;}1 {print $1,$2-1,$2,$3}' | tr -s " " "\t" > $DATA/coverage.bed
+if [ ! -f "$DATA/coverage.bed" ]
+then
+    echo "CREATING $DATA/coverage.bed"
+    for chrom in {1..22} X Y
+    do
+        wget -P $DATA ftp://ftp.broadinstitute.org/pub/ExAC_release/release0.3/coverage/Panel.chr$chrom.coverage.txt.gz
+    done
+    gzcat $DATA/Panel.chr*.coverage.txt.gz \
+        | awk '/^#/ {sub(/#.*/,"");getline;}1 {print $1,$2-1,$2,$3}' \
+        | tr -s " " "\t" \
+        > $DATA/coverage.bed
+else
+    echo "FOUND $DATA/coverage.bed"
+fi
 
 # bill's gtfs:
 
-for chrom in {1..22} X Y ; do wget -P $DATA http://fastademo.bioch.virginia.edu/pfam_dna/Homo_sapiens.chr$chrom.pfam.gtf; done
-for chrom in {1..22} X Y
-do
-sort -k2,2n $DATA/Homo_sapiens.chr$chrom.pfam.gtf | python rearrange.py | sort -k1,1 -k2,2n  > $DATA/chr$chrom.bed
-done
-cat $DATA/chr*.bed | sort -k1,1 -k2,2n > $DATA/all.bed
-rm $DATA/chr*
+if [ ! -f "$DATA/all.bed" ]
+then
+    echo "CREATING $DATA/all.bed"
+    for chrom in {1..22} X Y
+    do
+        wget -P $DATA http://fastademo.bioch.virginia.edu/pfam_dna/Homo_sapiens.chr$chrom.pfam.gtf
+    done
+    for chrom in {1..22} X Y
+    do
+        sort -k2,2n $DATA/Homo_sapiens.chr$chrom.pfam.gtf \
+        | python rearrange.py \
+        | sort -k1,1 -k2,2n  \
+        > $DATA/chr$chrom.bed
+    done
+    cat $DATA/chr*.bed | sort -k1,1 -k2,2n > $DATA/all.bed
+    rm $DATA/chr*
+else
+    echo "FOUND $DATA/all.bed"
+fi
