49d0e2b5ff30d6bddd9a357ec5b112f9ac51db91
Ryan Layer <ryan.layer@gmail.com>
2015-10-08 14:03:58 -0600

    use a dict instead of striding through the file

--- filterutrs.py
+++ filterutrs.py
@@ -1,73 +1,88 @@
 import string
 import sys
 import pybedtools
+import subprocess 
+import operator 
 
-class RecordExon(object):
-	def __init__(self, fields):
-		self.chr = fields[0]
-		self.start = fields[1]
-		self.end = fields[2]
-		self.crapandstrand = "\t".join(fields[3:6])
-		self.info=fields[6]
-		self.transid = self.info.replace("\"","").replace(";","").split(" ")[3]
+class Record(object):
+    def __init__(self, fields):
+        self.chr = fields[0]
+        self.start = int(fields[1])
+        self.end = int(fields[2])
+        self.crapandstrand = "\t".join(fields[3:6])
+        self.info=fields[6]
+        self.transid = self.info.replace("\"","").replace(";","").split()[3]
+    def __str__(self):
+        return '\t'.join([self.chr, \
+                          str(self.start),\
+                          str(self.end), \
+                          self.crapandstrand,\
+                          self.info])
 
-class RecordUtrs(object):
-	def __init__(self,fields):
-		self.chr = fields[0]
-		self.start = fields[1]
-		self.end = fields[2]
-		self.crapandstrand = "\t".join(fields[3:6])
-		self.info = fields[6]
-		self.transid = self.info.replace("\"","").replace(";","").split(" ")[3]
+E = {}
+U = {}
 
-f1=open(sys.argv[1],"r")
-f2=open(sys.argv[2],"r")
-exons=f1.readline()
-utrs=f2.readline()
-e_bed=''
-u_bed=''
+f = open(sys.argv[1],"r")
+for l in f:
+    e = Record(l.rstrip().split("\t"))
+    if not e.transid in E:
+        E[e.transid] = []
+    E[e.transid].append(e)
+f.close()
 
-while exons and utrs:
-	e=RecordExon(exons.split("\t"))
-	u=RecordUtrs(utrs.split("\t"))
-	old_e_trans=e.transid
-	old_u_trans=u.transid
-	if e.transid == u.transid:
-		while e.transid == old_e_trans:
-			e_bed=e_bed+e.chr+"\t"+e.start+"\t"+e.end+"\t"+e.crapandstrand+"\t"+e.info+"\n"
-			exons=f1.readline()
-			try:
-				e=RecordExon(exons.split("\t"))
-			except IndexError:
-				old_e_trans=None
-		while u.transid == old_u_trans:
-			u_bed=u_bed+u.chr+"\t"+u.start+"\t"+u.end+"\n"
-			utrs=f2.readline()
-			try:
-				u=RecordUtrs(utrs.split("\t"))
-			except IndexError:
-				old_u_trans=None
-		a=pybedtools.BedTool(e_bed,from_string=True)
-		b=pybedtools.BedTool(u_bed,from_string=True)
-		bed=str(a.subtract(b))
-		old_chrom=None
-		old_start=None
-		old_end=None
-		for i in bed.splitlines():
-			i=i.split()
-			chrom=i[0];start=i[1];end=i[2];crapandstrand="\t".join(i[3:6]);info=" ".join(i[6:len(i)])
-			old_chrom=chrom
-			old_start=start
-			old_end=end
-			sys.stdout.write("\t".join([chrom,start,end,crapandstrand,info])+"\n")
-		e_bed=''
-		u_bed=''
-	elif e.transid > u.transid:
-		utrs=f2.readline()
-	elif e.transid < u.transid:
-		exons=f1.readline()
-	if not utrs or not exons:
-		break
+f = open(sys.argv[2],"r")
+for l in f:
+    u = Record(l.rstrip().split("\t"))
+    if not u.transid in U:
+        U[u.transid] = []
+    U[u.transid].append(u)
+f.close()
 
-f1.close()
-f2.close()
+total_dropped = 0
+
+for e_transid in E:
+    if not e_transid in U:
+        x=1
+        for e in E[e_transid]:
+            print e
+    else:
+        f = open('u.tmp', 'w')
+        f.write('\n'.join( \
+                [str(u) for u in sorted(U[e_transid], \
+                                        key=lambda x:(x.chr,x.start,x.end))]))
+        f.close()
+
+        f = open('e.tmp', 'w')
+        f.write('\n'.join( \
+                [str(e) for e in sorted(E[e_transid], \
+                                        key=lambda x:(x.chr,x.start,x.end))]))
+
+        f.close()
+
+        f = open('r.tmp', 'w')
+        p = subprocess.Popen('bedtools subtract -sorted -a e.tmp -b u.tmp',
+                             shell=True,
+                             stdout=f)
+        p.wait()
+        f.close()
+
+        f = open('r.tmp', 'r')
+        num_lines = sum(1 for line in f)
+        f.close()
+
+        total_dropped += len(E[e_transid]) - num_lines
+
+#        if (len(E[e_transid]) != num_lines):
+#            sys.stderr.write('Warning: ' + \
+#                             str(len(E[e_transid])) + \
+#                             ' exons in transcript and ' + \
+#                             str(num_lines) + \
+#                             ' exons after UTR removal for ' + \
+#                             e_transid + '\n')
+
+        f = open('r.tmp', 'r')
+        for l in f:
+            print l.rstrip()
+        f.close()
+
+sys.stderr.write('A total of ' + str(total_dropped) + 'exons removed' + '\n')
