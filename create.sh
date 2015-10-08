e2b8394f5a9171946310178c70de022719bd13d8
Brent Pedersen <bpederse@gmail.com>
2015-10-08 16:18:38 -0600

    more tests

--- create.sh
+++ create.sh
@@ -19,14 +19,15 @@ awk 'NR==FNR{a[$2]}$29 in a{print $0}' $DATA/transcripts.txt $DATA/foo.bed | tr 
 
 python make-doms.py <(awk '{if ($15==$29) print}' $DATA/foo.bed | tr -s " " "\t" | cut -f -45) | sort -k1,1 -k2,2n > $DATA/doms-any-coverage.bed
 
-bedtools intersect -a <(cat $DATA/doms-any-coverage.bed | tr -s "\t" " " | cut -d " " -f 1-45 | tr -s " " "\t") -b <(awk '{if ($4>=5) print}' $DATA/coverage.bed) -wa -wb -sorted \
+bedtools intersect -a <(cat $DATA/doms-any-coverage.bed | tr -s "\t" " " | cut -d " " -f 1-45 | tr -s " " "\t") -b <(awk -v cov=$COV '{if ($4>=cov) print}' $DATA/coverage.bed) -wa -wb -sorted \
 | awk '{ct[$1 $2 $3 $45]++; len[$1 $2 $3 $45]=$4; row[$1 $2 $3 $45]=$0} END {for (i in ct) print row[i],(ct[i]==0 ? ct[i]=0: ct[i]), (len[i]==0 ? len[i]=1: len[i]),ct[i]/(len[i]==0 ? len[i]=1: len[i])}' \
 | tr -s " " "\t" | cut -f 1-45,50- > $DATA/doms.bed
 
 # nodoms
 perl -pe 's/tag\s*\S*?(?=\n|\s)//g' $DATA/exons_sans_utrs.bed | perl -pe 's/ccds_id\s*\S*?(?=\n|\s)//g' | perl -pe 's/"|;//g' | awk 'NR==FNR{a[$2]}$10 in a{print}' $DATA/transcripts.txt - | tr -s " " "\t" | sort -k10,10 > $DATA/foo2
-awk '{split($15,trans,","); for (i in trans) print $1,$2,$3,$13,$25,trans[i],$43}' $DATA/doms.bed | sort -k6,6 | tr -s " " "\t"  > $DATA/foo3
-python nodom.py $DATA/foo2 $DATA/foo3 > $DATA/nodoms-any-coverage.bed; rm $DATA/foo2 $DATA/foo3
+awk '{split($15,trans,","); for (i in trans) print $1,$2,$3,$13,$25,trans[i],$43}' $DATA/doms-any-coverage.bed | sort -k6,6 | tr -s " " "\t"  > $DATA/doms-split.bed
+python nodom.py $DATA/foo2 $DATA/doms-split.bed > $DATA/nodoms-any-coverage.bed; 
+rm $DATA/foo2 $DATA/foo3
 
 bedtools intersect -a <(sort -k1,1 -k2,2n $DATA/nodoms-any-coverage.bed) -b <(awk '{if ($4>=5) print}' $DATA/coverage.bed) -wa -wb -sorted \
 | awk '{ct[$1 $2 $3 $8 $24]++; len[$1 $2 $3 $8 $24]=$4; row[$1 $2 $3 $8 $24]=$0} END {for (i in row) print row[i],(ct[i]==0 ? ct[i]=0: ct[i]),(len[i]==0 ? len[i]=1: len[i]),ct[i]/(len[i]==0 ? len[i]=1: len[i])}' \
--- test.sh
+++ test.sh
@@ -1,14 +1,21 @@
 DATA=$1
 
-exp=$(cut -f 2 $DATA/appris_data.principal.txt | sort -u | wc -l | cut -d " " -f 1)
-obs=$(wc -l < $DATA/transcripts.txt)
 
-echo "pmodel.t1... test that number of uniq genes before filtering on maximum length is equal to the number in transcripts.txt"
-if [[ $exp != $obs ]]; then
-	echo "FAIL the number of lines in transcripts should equal the number of uniq genes from appris"
+check(){
+	obs=$1
+	exp=$2
+
+if [[ "$exp" != "$obs" ]]; then
+	echo "FAIL"
 else
 	echo "    OK"
 fi
+}
+
+echo "pmodel.t1... test that number of uniq genes before filtering on maximum length is equal to the number in transcripts.txt"
+exp=$(cut -f 2 $DATA/appris_data.principal.txt | sort -u | wc -l | cut -d " " -f 1)
+obs=$(wc -l < $DATA/transcripts.txt)
+check $obs $exp
 
 
 echo "pmodel.t2... test that the nodoms don't intersect the doms"
@@ -16,9 +23,16 @@ echo "pmodel.t2... test that the nodoms don't intersect the doms"
 obs=$(bedtools intersect -a $DATA/doms-any-coverage.bed -b $DATA/nodoms-any-coverage.bed -wo \
 	| python test-different-transcripts.py)
 exp=""
+check $obs $exp
 
-if [[ "$exp" != "$obs" ]]; then
-	echo "FAIL"
-else
-	echo "    OK"
-fi
+
+echo "pmodel.t3... test that exons-(doms+nodoms) gives empty results"
+
+obs=$(bedtools subtract -a <(cat $DATA/doms-any-coverage.bed $DATA/nodoms-any-coverage.bed|cut -f 1-22) -b $DATA/exons_sans_utrs.bed)
+exp=""
+check $obs $exp
+
+echo "pmodel.t4... test that (doms+nodoms)-exons gives empty results"
+obs=$(bedtools subtract -b <(cat $DATA/doms-any-coverage.bed $DATA/nodoms-any-coverage.bed | cut -f 1-22) -a <(cut -f 2 $DATA/transcripts.txt | grep -wFf - $DATA/exons_sans_utrs.bed))
+exp=""
+check $obs $exp
