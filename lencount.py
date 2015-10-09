93d812975666ae5dafe4cd49acc95e3cd1b09610
James Havrilla <u1021864@kingspeak1.wasatch.peaks>
2015-10-09 16:08:57 -0600

    transid stuff added

--- lencount.py
+++ lencount.py
@@ -8,20 +8,21 @@ class Record1(object):
 		self.start = fields[1]
 		self.end = fields[2]
 		self.domain = fields[3]
-		self.autoreg = fields[4]
-		self.geneid = fields[5]
-		self.gene = fields[6]
-		self.exonid = fields[7]
-		self.uniqid = fields[8]
-		self.len = fields[9]
-		self.covratio = fields[10]
+		self.transid = fields[4]
+		self.autoreg = fields[5]
+		self.geneid = fields[6]
+		self.gene = fields[7]
+		self.exonid = fields[8]
+		self.uniqid = fields[9]
+		self.len = fields[10]
+		self.covratio = fields[11]
 
 row=[]
 length={}
 for line in sys.stdin:
 	fields=line.rstrip().split("\t")
 	r_=Record1(fields)
-	row.append([r_.chr,r_.start,r_.end,r_.domain,r_.gene,r_.autoreg,r_.uniqid,r_.covratio])
+	row.append([r_.chr,r_.start,r_.end,r_.domain,r_.transid,r_.gene,r_.autoreg,r_.uniqid,r_.covratio])
 	key=base64.encodestring(r_.gene+r_.autoreg)
 	try:
 		length[key]=int(length[key])+int(r_.len)
