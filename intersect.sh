780bf13d72635cfbde5372ee218cfee5a311d2da
Jim Havrilla <jmh2tt@204-99-169-120.uguest.utah.edu>
2015-10-14 09:17:29 -0600

    awk

--- intersect.sh
+++ intersect.sh
@@ -15,15 +15,14 @@ fi
 #intersect
 
 bedtools intersect -a <(cut -f 1,2,3,11,15,25,27,33,43,45,47,48 $DATA/doms.bed \
-	| sort -k1,1 -k2,2n -k3,3n) -b $DATA/VEPEXAC3filter.vcf.gz -sorted -wb \
-    | cut -f 1,2,3,4,5,6,8,10,11,12,16,17,20 \
-	| python int-filter.py -d | sort -k1,1 -k2,2n > $DATA/domintfilter.bed
+		| sort -k1,1 -k2,2n -k3,3n) -b $DATA/VEPEXAC3filter.vcf.gz -sorted -wb \
+		| awk '{OFS="\t"; print $1,$14-1,($14-1)+length($16),$4,$5,$6,$8,$10,$11,$12,$16,$17,$20}' \
+		| python int-filter.py -d | sort -k1,1 -k2,2n > $DATA/domintfilter.bed
 
 bedtools intersect -a <(cut -f 1,2,3,8,12,24,26,27 $DATA/nodoms.bed \
-    | tr -s " " "\t" | sort -k1,1 -k2,2n -k3,3n) -b $DATA/VEPEXAC3filter.vcf.gz -sorted -wb \
-    | cut -f 1,2,3,4,5,6,7,8,12,13,16 \
-    | python int-filter.py -n | sort -k1,1 -k2,2n > $DATA/nodomintfilter.bed
-
-# assign types to impacts
+		| tr -s " " "\t" | sort -k1,1 -k2,2n -k3,3n) -b $DATA/VEPEXAC3filter.vcf.gz -sorted -wb \
+		| awk '{OFS="\t"; print $1,$10-1,($10-1)+length($12),$4,$5,$6,$7,$8,$12,$13,$16;}' \
+		| python int-filter.py -n | sort -k1,1 -k2,2n > $DATA/nodomintfilter.bed
+# assign types to impacts in int-filter.py
 
 cat $DATA/domintfilter.bed $DATA/nodomintfilter.bed | cat <(printf "#chr,start,end,ref,alt,pfamA_id,autoreg,uniqid,length_of_region,covratio,gene_symbol,maf,impact,type,codons,amino_acids,gene_id_csq,gene_symbol_csq,transcript_id_csq,exon_number_csq,polyphen,sift,protein_position,biotype\n") <(awk 'NR==FNR{a[$2]}$19 in a{print $0}' $DATA/transcripts.txt - | sort -k11,11 -k7,7) > $DATA/allintfilter.bed
