d657184f30b6f31e9c5fb5b676aeaf282a7d31e1
Jim Havrilla <jmh2tt@cphgs-C02N30EZFD59.local>
2016-01-26 02:16:16 -0700

    lollipop fix, added executable python script

--- lollipop.sh
+++ lollipop.sh
@@ -1,11 +1,14 @@
 #lollipop.sh
 DATA=$1; OUT=$2; GENE=$3; MOD=$4; MAF1=$5; MAF2=$6
-bash query.sh $DATA/var.db $GENE $MOD $MAF1 $MAF2 | python formatvar.py | sort -k6,6 -k8,8 | grep -v -w "na" > $OUT/$GENE'fooint'
+#sort -k6,6 -k8,12 $DATA/allintfilter.bed | sed '1d' | sed -e '/[[:space:]]na[[:space:]]/ {d;}' > $DATA/lollipop.bed
+grep $GENE $DATA/lollipop.bed > $OUT/$GENE'fooint'
 if [[ $MOD -eq g && $MAF1 -eq 0 ]]
 then
 	OF=$OUT/$GENE
 else 
 	OF=$OUT/$GENE.$MOD.$MAF1.$MAF2
 fi
-lollipops -m=1 -o=$OF.svg $(perl -pe 's/(\S*\s){11}(\S*)\s(\S*\s){3}(\S*)\s(\S*\s){6}(\S*).*\n/$4,$6,$2 /g' $OUT/$GENE'fooint' | perl -pe 's/(\w*|\*|\.)\/?(\S*),(\d*-?\d*)\/\d*,(\d*\.\d*e?-?\d*)/$1$3$2 $4/g' | paste <(printf $GENE) - )
+expr=$(perl -pe 's/(\S*\s){11}(\S*)\s(\S*\s){3}(\S*)\s(\S*\s){6}(\S*).*\n/$4 $6 $2 /g' $OUT/$GENE'fooint' | perl -pe 's/(\w*|\*|\.)\/?(\S*) (\?|\d*-?\d*)\/\d* (\d*\.\d*e?-?\d*)/$1$3$2 $4/g' | paste <(printf $GENE) - )
+echo $expr > foo
+lollipops -m=1 -o=$OF.svg $expr
 convert $OF.svg $OF.png # uses ImageMagick convert
